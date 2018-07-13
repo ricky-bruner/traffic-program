f322c310adeb2b0f27451747da468d606f0688d2
ricky-bruner <alteredecho07@yahoo.com>
2018-07-13 17:12:45 -0500

    made progress

--- main.js
+++ main.js
@@ -10,28 +10,64 @@ const mainQuestions = [
     "Do you believe that lane markings are more of a 'suggestion' than a rule?",
     "Do you believe that it is okay pass others in the shoulder?"
 ];
+const answer = "NO, YOU SHOULD NOT BE DRIVING DURING RUSH HOUR!";
+const bestAnswer = "Good Job! But you still shouldn't be driving during rush hour!"
 const mainContent = document.querySelector("#place-content")
 const makeButton = document.createElement("button");
-// const input = document.createElement("input");
-// input.type = "radio"
-function addElement(element){
+
+function addElement(element) {
     let newElement = document.createElement(element);
     mainContent.appendChild(newElement);
 }
-function addTypeElement(element, parameter){
+function addTypeElement(element, parameter) {
     let newElement = document.createElement(element);
     newElement.type = parameter;
     mainContent.appendChild(newElement);
 }
-addElement("h1");
-addTypeElement("input", "radio");
-addElement("p");
-addTypeElement("input", "radio");
-addElement("p");
-
-function addText(element, text){
-    element = document.querySelectorAll(element);
-    element.textContent(text);
-    element.appendChild();
+function addText(element, text, index) {
+    const getElement = document.querySelectorAll(element)[index]
+    getElement.textContent += text;
 }
-addText("p","Yes");
+function removeElement(element, index){
+    let removeElement = document.querySelectorAll(element)[index]
+    mainContent.removeChild(removeElement);
+}
+addTypeElement("button", "submit");
+addText("button", "Take Quiz", 0);
+const button = document.querySelector("button");
+button.addEventListener('click', generateQuestion);
+function generateQuestion() {
+    removeElement("button",0);
+    addElement("h1");
+    addText("h1",mainQuestions[0],0)
+    addTypeElement("input", "radio");
+    addElement("p");
+    addTypeElement("input", "radio");
+    addElement("p");
+    addTypeElement("button", "submit");
+    addText("p", "Yes", 0);
+    addText("p", "No", 1);
+    addText("button", "Submit", 0)
+}
+// const p = document.querySelector("p");
+
+
+
+
+function askQuestion(array) {
+    let question;
+    for (let i = 0; i < array.length; i++) {
+        question = array[i];
+        console.log(question);
+    }
+    return question;
+}
+askQuestion(mainQuestions);
+let getQuestion = askQuestion(mainQuestions);
+addText("h1", getQuestion, 0);
+
+function askNextQuestion() {
+    generateQuestion();
+}
+
+button.addEventListener('click', askNextQuestion);
