2d490c2a3d5179718925a80f6129e959d48bc694
ricky-bruner <alteredecho07@yahoo.com>
2018-07-16 09:30:25 -0500

    updated readme and js code

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# A simple traffic program
+# A simple traffic program?
+
+ I wanted to test myself to 'think' through the process of making a simple app that will generate questions and respond to the user clicking a submit button.
+
+ So, I hate traffic. I hate Interstate 24. I travel this road twice a day, at the worst time of day. Life is super fair like that 😂😂😂
+
+ An idea came to me to make a sarcastic app that quizzes the user on their driving quirks related to traffic scenarios, and no matter what, tell the user to get off the road during rush hour. It seemed funny at the time.
+
+ Then came the code...
+
+ ## Other Challenges!
+
+ During the course of constantly breaking and reorganizing this program, I also wanted to challenge myself to make re-usable code. I discovered early on that I was using a function for E.V.E.R.Y.T.H.I.N.G.
+
+ So why not try to make the code dry as well? So I came up with these to be used throughout the code repeatedly:
+ ```javascript
+ function addElement(element) {
+    let newElement = document.createElement(element);
+    mainContent.appendChild(newElement);
+}
+```
+this makes basic html elements and adds them to a div I hardcoded into the index.html
+```javascript
+function addText(element, text, index) {
+    const getElement = document.querySelectorAll(element)[index]
+    getElement.textContent += text;
+}
+```
+This one adds text to the buttons and h1 elements
+```javascript
+function addClass(element, index, className){
+    let grabElement = document.querySelectorAll(element);
+    grabElement[index].setAttribute('class', className);
+}
+```
+And that final function adds CSS classes to my generated elements.
+
+### Hopes and Dreams
+
+Currently, the code will generate new questions. I hope in the near future to solve the placement of the conditional to get it working and make the app functions how originally envisioned! Thanks for checking it out, pleasemjust regard this as a simple representation of where my ubderstanding of Javascript is at week 2 of NSS!
--- main.js
+++ main.js
@@ -32,42 +32,95 @@ function removeElement(element, index){
     let removeElement = document.querySelectorAll(element)[index]
     mainContent.removeChild(removeElement);
 }
+function addClass(element, index, className){
+    let grabElement = document.querySelectorAll(element);
+    grabElement[index].setAttribute('class', className);
+}
 addTypeElement("button", "submit");
 addText("button", "Take Quiz", 0);
 const button = document.querySelector("button");
+let i = 0;
 button.addEventListener('click', generateQuestion);
+
 function generateQuestion() {
     removeElement("button",0);
     addElement("h1");
-    addText("h1",mainQuestions[0],0)
-    addTypeElement("input", "radio");
-    addElement("p");
-    addTypeElement("input", "radio");
-    addElement("p");
+    addText("h1",mainQuestions[i],0);
+    i++;
     addTypeElement("button", "submit");
-    addText("p", "Yes", 0);
-    addText("p", "No", 1);
-    addText("button", "Submit", 0)
-}
-// const p = document.querySelector("p");
-
-
-
-
-function askQuestion(array) {
-    let question;
-    for (let i = 0; i < array.length; i++) {
-        question = array[i];
-        console.log(question);
+    addClass("button", 0, "yes-button");
+    const yesButton = document.getElementsByClassName('yes-button')[0];
+    yesButton.addEventListener('click', generateNextQuestion);
+    addTypeElement("button", "submit");
+    addClass("button", 1, "no-button");
+    const noButton = document.getElementsByClassName('no-button')[0];
+    noButton.addEventListener('click', generateNextQuestion);
+    addText("button", "Yes", 0);
+    addText("button", "No", 1);
+    function generateNextQuestion(){
+        if(yesButton.clicked === true){
+            removeElement("button", 0);
+            removeElement("button", 1);
+            removeElement("h1", 0);
+            addElement("h1");
+            addText("h1", answer, 0);
+        } else {
+            removeElement("h1", 0);
+            addElement("h1");
+            addText("h1",mainQuestions[i],0);
+            i++
+        }
+        
     }
-    return question;
+    
+    // if(yesButton.clicked === true){
+    //     removeElement("button", 0);
+    //     removeElement("button", 1);
+    //     removeElement("h1", 0);
+    //     addElement("h1");
+    //     addText("h1", answer, 0);
+    // } else {
+    //     generateNextQuestion();
+    // }
 }
-askQuestion(mainQuestions);
-let getQuestion = askQuestion(mainQuestions);
-addText("h1", getQuestion, 0);
 
-function askNextQuestion() {
-    generateQuestion();
-}
+// function generateNextQuestion(){
+//     removeElement("h1", 0);
+//     addElement("h1");
+//     addText("h1",mainQuestions[i],0);
+//     i++
+// }
+// const yesButton = document.getElementsByClassName('yes-button')[0];
+// yesButton.addEventListener('click', generateNextQuestion);
+// if(yesButton.clicked === true){
+//     removeElement("button", 0);
+//     removeElement("button", 1);
+//     removeElement("h1", 0);
+//     addElement("h1");
+//     addText("h1", answer, 0);
+// } else {
+//     generateNextQuestion();
+// }
 
-button.addEventListener('click', askNextQuestion);
+
+
+
+
+
+// function askQuestion(array) {
+//     let question;
+//     for (let i = 0; i < array.length; i++) {
+//         question = array[i];
+//         console.log(question);
+//     }
+//     return question;
+// }
+// askQuestion(mainQuestions);
+// let getQuestion = askQuestion(mainQuestions);
+// addText("h1", getQuestion, 0);
+
+// function askNextQuestion() {
+//     generateQuestion();
+// }
+
+// button.addEventListener('click', askNextQuestion);
